898aec81a1461617580d089d540a327d6ededd99
Vaibhav Meshram <VMeshram@vaibbys-MacBook-Pro.local>
2018-09-26 15:08:44 +0530

    aaa

--- jay/jay/AppDelegate.swift
+++ jay/jay/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //test
+        //asd
         // Override point for customization after application launch.
         return true
     }
